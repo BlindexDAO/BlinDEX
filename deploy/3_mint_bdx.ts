7df0f5d0d9d8b5c86256a6e926c1f3ac606e2975
Andrzej Gis <andrzej.gis@widelab.co>
2021-07-11 02:54:52 +0200

    fixed initial bdx number

--- deploy/3_mint_bdx.ts
+++ deploy/3_mint_bdx.ts
@@ -1,19 +1,20 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { BDXShares } from '../typechain/BDXShares';
-import { BigNumber } from 'ethers';
+import { to_d18 } from '../utils/Helpers';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const [ deployer ] = await hre.ethers.getSigners();
+    const treasury = await hre.ethers.getNamedSigner("COLLATERAL_FRAX_AND_FXS_OWNER");
 
     const bdxInstance = await hre.ethers.getContract("BDXShares") as unknown as BDXShares;
 
-    // todo ag why?!?!? we mint new BDX in multiple places!
-    // mint all BDX up front
-    await (await bdxInstance.connect(deployer).mint(
+    // mint half of the BDX up front, to reseve for staking rewards
+    await bdxInstance.connect(deployer).mint(
         '0x0000000000000000000000000000000000000000',
-        (await hre.ethers.getNamedSigner("COLLATERAL_FRAX_AND_FXS_OWNER")).address,
-        BigNumber.from(21).mul(BigNumber.from(10).pow(6 + 18)))).wait();
+        treasury.address,
+        to_d18(21).mul(1e6).div(2)
+    );
 
     // One time migration
     return true;
