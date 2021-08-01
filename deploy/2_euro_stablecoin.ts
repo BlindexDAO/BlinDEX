6e09f6d1367dde580b69a3fa41bae6dde0de250a
Andrzej Gis <andrzej.gis@widelab.co>
2021-08-02 01:36:26 +0200

    upgradable bd stable pool

--- deploy/2_euro_stablecoin.ts
+++ deploy/2_euro_stablecoin.ts
@@ -4,6 +4,7 @@ import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
 import * as constants from '../utils/Constants'
 import { to_d18 } from '../utils/Helpers';
+import { BdStablePool } from '../typechain/BdStablePool';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const deployer = (await hre.getNamedAccounts()).DEPLOYER_ADDRESS;
@@ -36,30 +37,50 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   console.log("BDEUR deployed to:", bdEur.address);
   
-  const bdeur_weth_BdStablePoolDeployment = await hre.deployments.deploy('BDEUR_WETH_POOL', {
-    from: deployer,
-    contract: 'BdStablePool',
-    args: [bdEur.address, bdx.address, constants.wETH_address[hre.network.name], deployer],
-    libraries: {
-      BdPoolLibrary: bdPoolLibraryDeployment.address
+  const bdeur_weth_BdStablePoolDeployment = await hre.deployments.deploy(
+    'BDEUR_WETH_POOL', 
+    {
+      from: deployer,
+      proxy: {
+        proxyContract: 'OptimizedTransparentProxy',
+      },
+      contract: 'BdStablePool',
+      args: [],
+      libraries: {
+        BdPoolLibrary: bdPoolLibraryDeployment.address
+      }
     }
-  });
+  );
   
-  console.log("BDEUR WETH Pool deployed to:", bdeur_weth_BdStablePoolDeployment.address);
+  const bdeur_weth_BdStablePool = await hre.ethers.getContract('BDEUR_WETH_POOL') as BdStablePool;
   
-  const bdeur_wbtc_BdStablePoolDeployment = await hre.deployments.deploy('BDEUR_WBTC_POOL', {
-    from: deployer,
-    contract: 'BdStablePool',
-    args: [bdEur.address, bdx.address, constants.wBTC_address[hre.network.name], deployer],
-    libraries: {
-      BdPoolLibrary: bdPoolLibraryDeployment.address
-    }
-  });
-  
-  console.log("BDEUR WBTC Pool deployed to:", bdeur_wbtc_BdStablePoolDeployment.address);
+  await bdeur_weth_BdStablePool.initialize(bdEur.address, bdx.address, constants.wETH_address[hre.network.name], deployer);
 
-  await (await bdEur.addPool(bdeur_weth_BdStablePoolDeployment.address)).wait()
-  await (await bdEur.addPool(bdeur_wbtc_BdStablePoolDeployment.address)).wait()
+  console.log("BDEUR WETH Pool deployed to:", bdeur_weth_BdStablePool.address);
+  
+  const bdeur_wbtc_BdStablePoolDeployment = await hre.deployments.deploy(
+    'BDEUR_WBTC_POOL', 
+    {
+      from: deployer,
+      proxy: {
+        proxyContract: 'OptimizedTransparentProxy',
+      },
+      contract: 'BdStablePool',
+      args: [],
+      libraries: {
+        BdPoolLibrary: bdPoolLibraryDeployment.address
+      }
+    }
+  );
+
+  const bdeur_wbtc_BdStablePool = await hre.ethers.getContract('BDEUR_WBTC_POOL') as BdStablePool;
+  
+  await bdeur_wbtc_BdStablePool.initialize(bdEur.address, bdx.address, constants.wBTC_address[hre.network.name], deployer);
+  
+  console.log("BDEUR WBTC Pool deployed to:", bdeur_wbtc_BdStablePool.address);
+
+  await (await bdEur.addPool(bdeur_weth_BdStablePool.address)).wait()
+  await (await bdEur.addPool(bdeur_wbtc_BdStablePool.address)).wait()
 
   await (await bdx.addBdStableAddress(bdEur.address)).wait()
 
