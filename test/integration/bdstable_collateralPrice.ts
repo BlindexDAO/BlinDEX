c8e432fffd926e80cbf58dbf499aec59c00ed908
Grzesiek Pikiel <grzesiek.pikiel@widelab.co>
2021-10-25 20:53:09 +0200

    Comment for clarity on big precision margin

--- test/integration/bdstable_collateralPrice.ts
+++ test/integration/bdstable_collateralPrice.ts
@@ -24,7 +24,7 @@ describe("Collateral price", () => {
         const wethPool = await getBdEuWethPool(hre);
 
         const expectedWethPrice = 3600; //ETH-EUR for 2021-10-25
-        const precision = 0.33;
+        const precision = 0.33; // Big margin due to high crypto price volatility - can update the test less often
         const wethInEurPrice = d12_ToNumber(await wethPool.getCollateralPrice_d12());
 
         console.log("wethInEurPrice: " + wethInEurPrice);
@@ -38,7 +38,7 @@ describe("Collateral price", () => {
         const wbtcPool = await getBdEuWbtcPool(hre);
 
         const expectedWbtcPrice = 54000; // BTC-EUR for 2021-10-25
-        const precision = 0.33;
+        const precision = 0.33; // Big margin due to high crypto price volatility - can update the test less often
         const wbtcInEurPrice = d12_ToNumber(await wbtcPool.getCollateralPrice_d12());
 
         console.log("wbtcInEurPrice: " + wbtcInEurPrice);
