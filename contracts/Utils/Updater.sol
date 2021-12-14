06c03b3cb2b334329ea22841a0a47d235f3ed9bf
tadeusz-cirocki <tadeusz.cirocki@widelab.co>
2021-12-14 15:09:07 +0100

    Update Updater.sol
    4 kinds of update

--- contracts/Utils/Updater.sol
+++ contracts/Utils/Updater.sol
@@ -2,12 +2,72 @@
 pragma solidity 0.6.12;
 
 import "../Oracle/SovrynSwapPriceFeed.sol";
+import "../Oracle/FiatToFiatPseudoOracleFeed.sol";
+import "../Oracle/UniswapPairOracle.sol";
+import "../BDStable/BDStable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Updater {
+contract Updater is Ownable {
+    function update(
+        address[] memory _sovrynOracles,
+        uint256[] memory _sovrynPrices,
+        address[] memory _fiatOracles,
+        uint256[] memory _fiatPrices,
+        address[] memory _uniswapOracles,
+        address[] memory _BDStables
+    ) external onlyUpdater {
 
-    function updateOraclesWithVerification(address[] memory _oracles, uint256[] memory _prices) public {
-        for (uint i=0; i<_oracles.length; i++) {
-            SovrynSwapPriceFeed(_oracles[i]).updateOracleWithVerification(_prices[i]);
+        require(
+            _sovrynOracles.length == _sovrynPrices.length,
+            "Each sovryn oracle address needs its price"
+        );
+        require(
+            _fiatOracles.length == _fiatPrices.length,
+            "Each fiat oracle address needs its price"
+        );
+
+        for (uint256 i = 0; i < _sovrynOracles.length; i++) {
+            SovrynSwapPriceFeed priceFeed = SovrynSwapPriceFeed(
+                _sovrynOracles[i]
+            );
+            if (priceFeed.shouldUpdateOracle()) {
+                priceFeed.updateOracleWithVerification(_sovrynPrices[i]);
+            }
         }
+
+        for (uint256 i = 0; i < _fiatOracles.length; i++) {
+            FiatToFiatPseudoOracleFeed priceFeed = FiatToFiatPseudoOracleFeed(
+                _fiatOracles[i]
+            );
+            priceFeed.setPrice(_fiatPrices[i]);
+        }
+
+        for (uint256 i = 0; i < _uniswapOracles.length; i++) {
+            UniswapPairOracle priceFeed = UniswapPairOracle(_uniswapOracles[i]);
+            if (priceFeed.shouldUpdateOracle()) {
+                priceFeed.updateOracle();
+            }
+        }
+        
+        for (uint256 i = 0; i < _BDStables.length; i++) {
+            BDStable priceFeed = BDStable(_BDStables[i]);
+            priceFeed.refreshCollateralRatio();
+        }
+    }
+
+    function setUpdater(address newUpdater) external onlyOwner {
+        require(
+            newUpdater != address(0),
+            "Updater cannot be set to the zero address"
+        );
+
+        address oldUpdater = updater;
+        updater = newUpdater;
+        emit UpdaterChanged(oldUpdater, updater);
+    }
+
+    modifier onlyUpdater() {
+        require(msg.sender == updater, "You're not updater");
+        _;
     }
 }
