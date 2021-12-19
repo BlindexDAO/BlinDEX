460d2e66f231ded2fb181f138313dfd4b731de38
Tom Keidar <tom.keidar@portis.io>
2021-12-19 22:43:40 +0200

    transfer 15% (3,150,000) of the BDX tokens to the locking contract

---
+++ deploy/1_4_transfer_bdx_to_locking_contract.ts
@@ -0,0 +1,21 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+import * as constants from '../utils/Constants'
+import { getBdx, getTreasury } from '../utils/DeployedContractsHelpers';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    console.log("starting: transfer BDX to locking contract");
+
+    const treasury = await getTreasury(hre);
+    const bdx = await getBdx(hre);
+    const tx = await bdx.connect(treasury).transfer(constants.rskLockingContractAddress, constants.bdxLockAmount)
+    await tx.wait();
+    
+    console.log("finished: transfer BDX to locking contract");
+    return true;
+};
+
+func.id = __filename
+func.tags = ['LockBDX'];
+func.dependencies = ['BdxMint'];
+export default func;
