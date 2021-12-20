6f345bcaca18fb2e65cf3b460041a16372d4732f
Or Kazaz <or.kazaz89@gmail.com>
2021-12-20 18:59:46 +0200

    Added github action workflow

--- contracts/Oracle/ICryptoPairOracle.sol
+++ contracts/Oracle/ICryptoPairOracle.sol
@@ -1,12 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.6 <=0.6.12;
+pragma solidity ^0.6.12;
 
 interface ICryptoPairOracle {
-    
-    function consult(address tokenIn, uint256 amountIn)
-        external
-        view
-        returns (uint amountOut);
+    function consult(address tokenIn, uint256 amountIn) external view returns (uint256 amountOut);
 
     function updateOracle() external;
 
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,22 +1,22 @@
-// Dirty hack to enforce hardhat-deploy-ethers type precedence over hardhat-ethers, while making hardhat-deploy-ethers extendEnvironment function execute last 
+// Dirty hack to enforce hardhat-deploy-ethers type precedence over hardhat-ethers, while making hardhat-deploy-ethers extendEnvironment function execute last
 // import type * as deployEthers from "hardhat-deploy-ethers";
 import type * as deployEthers from "hardhat-deploy-ethers";
 
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-ethers";
-import '@openzeppelin/hardhat-upgrades';
+import "@openzeppelin/hardhat-upgrades";
 import "hardhat-deploy-ethers";
-import 'hardhat-deploy';
-import '@typechain/hardhat';
+import "hardhat-deploy";
+import "@typechain/hardhat";
 import "@nomiclabs/hardhat-waffle";
-import dotenv from 'dotenv'
-import * as setupTasks from "./tasks/setup"
-import * as maintenanceTasks from "./tasks/mainternace"
-import * as developmentTasks from "./tasks/development"
-import * as feConfig from "./tasks/config"
+import dotenv from "dotenv";
+import * as setupTasks from "./tasks/setup";
+import * as maintenanceTasks from "./tasks/mainternace";
+import * as developmentTasks from "./tasks/development";
+import * as feConfig from "./tasks/config";
 
-const path = require('path');
-const envPath = path.join(__dirname, './.env');
+const path = require("path");
+const envPath = path.join(__dirname, "./.env");
 dotenv.config({ path: envPath });
 
 developmentTasks.load();
@@ -29,36 +29,29 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {
       forking: {
-        url: process.env.MAINNET_URL!
+        url: process.env.MAINNET_URL!,
       },
       accounts: {
         mnemonic: process.env.MNEMONIC_PHRASE!,
-        accountsBalance: '100000000000000000000000',
+        accountsBalance: "100000000000000000000000",
       },
-      chainId: 1337
+      chainId: 1337,
     },
     mainnetFork: {
-      url: 'http://localhost:8545',
+      url: "http://localhost:8545",
       timeout: 60000,
-      gas: 10_000_000
+      gas: 10_000_000,
     },
     kovan: {
-      url: 'https://kovan.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
-      accounts: [
-        process.env.USER_DEPLOYER_PRIVATE_KEY!,
-        process.env.USER_TREASURY_PRIVATE_KEY!
-      ],
-      timeout: 240000
+      url: "https://kovan.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+      accounts: [process.env.USER_DEPLOYER_PRIVATE_KEY!, process.env.USER_TREASURY_PRIVATE_KEY!],
+      timeout: 240000,
     },
     rsk: {
-      url: 'https://public-node.rsk.co',
-      accounts: [
-        process.env.USER_DEPLOYER_PRIVATE_KEY!,
-        process.env.USER_TREASURY_PRIVATE_KEY!,
-        process.env.USER_BOT_PRIVATE_KEY!
-      ],
+      url: "https://public-node.rsk.co",
+      accounts: [process.env.USER_DEPLOYER_PRIVATE_KEY!, process.env.USER_TREASURY_PRIVATE_KEY!, process.env.USER_BOT_PRIVATE_KEY!],
       timeout: 6_000_000,
-      gasPrice: 79240000
+      gasPrice: 79240000,
     },
   },
   solidity: {
@@ -68,63 +61,65 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200
-          }
-        }
+            runs: 200,
+          },
+        },
       },
     ],
   },
   mocha: {
-    timeout: 20000000
+    timeout: 20000000,
   },
   typechain: {
     outDir: "typechain",
     target: "ethers-v5",
     externalArtifacts: [
-      './node_modules/@uniswap/v2-core/build/UniswapV2Pair.json',
-      './node_modules/@uniswap/v2-core/build/UniswapV2Pair__factory.json',
-      './node_modules/@uniswap/v2-core/build/UniswapV2Factory.json',
-      './node_modules/@uniswap/v2-core/build/UniswapV2Factory__factory.json',
-      './node_modules/@uniswap/v2-periphery/build/UniswapV2Router02.json',
-      './node_modules/@uniswap/v2-periphery/build/UniswapV2Router02__factors.json'
-    ]
+      "./node_modules/@uniswap/v2-core/build/UniswapV2Pair.json",
+      "./node_modules/@uniswap/v2-core/build/UniswapV2Pair__factory.json",
+      "./node_modules/@uniswap/v2-core/build/UniswapV2Factory.json",
+      "./node_modules/@uniswap/v2-core/build/UniswapV2Factory__factory.json",
+      "./node_modules/@uniswap/v2-periphery/build/UniswapV2Router02.json",
+      "./node_modules/@uniswap/v2-periphery/build/UniswapV2Router02__factors.json",
+    ],
   },
   external: {
-    contracts: [{
-      artifacts: 'node_modules/@uniswap/v2-core/build'
-    }, {
-      artifacts: 'node_modules/@uniswap/v2-periphery/build'
-    }]
+    contracts: [
+      {
+        artifacts: "node_modules/@uniswap/v2-core/build",
+      },
+      {
+        artifacts: "node_modules/@uniswap/v2-periphery/build",
+      },
+    ],
   },
   namedAccounts: {
-
     DEPLOYER: {
-      default: 0
+      default: 0,
     },
     TREASURY: {
-      default: 1
+      default: 1,
     },
     BOT: {
-      default: 2
+      default: 2,
     },
     DEV_TREASURY: {
-      default: 3
+      default: 3,
     },
     TEST1: {
-      default: 4
+      default: 4,
     },
     TEST2: {
-      default: 5
+      default: 5,
     },
     TEST_VESTING_SCHEDULER: {
-      default: 6
+      default: 6,
     },
     TEST_VESTING_REWARDS_PROVIDER: {
-      default: 7
-    }
-  }
+      default: 7,
+    },
+  },
 };
 
-export default config
+export default config;
 
 //Make hardhat runs faster by specifying TS_NODE_TRANSPILE_ONLY=1 (https://hardhat.org/guides/typescript.html#performance-optimizations)
