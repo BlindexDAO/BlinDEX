54493713f2a16abe6fd2abda30674c6a1d334c2f
AndrzejGisWideLab <79972464+AndrzejGisWideLab@users.noreply.github.com>
2021-12-27 00:35:42 +0100

    F/lago 89 improve oracles reset (#85)
    * lago-89 added uniswap oracles test
    
    * Prettified code!
    
    * formatted code
    
    * Update test/integration/uniswap-oracles.ts
    
    * lago-89 improved tests setup to use moment
    
    Co-authored-by: AndrzejGisWideLab <AndrzejGisWideLab@users.noreply.github.com>

--- tasks/mainternace.ts
+++ tasks/mainternace.ts
@@ -9,12 +9,11 @@ import {
   getUniswapFactory,
   getUniswapPair,
   getUniswapPairOracle,
-  getUniswapRouter,
   getWeth
 } from "../utils/DeployedContractsHelpers";
 import { UniswapV2Pair } from "../typechain/UniswapV2Pair";
-import { bigNumberToDecimal, d12_ToNumber, d18_ToNumber, numberToBigNumberFixed, to_d12, to_d18 } from "../utils/NumbersHelpers";
-import { getPools, resetUniswapPairsOracles, tokensDecimals, updateUniswapPairsOracles } from "../utils/UniswapPoolsHelpers";
+import { bigNumberToDecimal, d12_ToNumber, d18_ToNumber, to_d12, to_d18 } from "../utils/NumbersHelpers";
+import { getPools, tokensDecimals, updateUniswapPairsOracles, resetUniswapPairsOracles } from "../utils/UniswapPoolsHelpers";
 import { BDStable } from "../typechain/BDStable";
 import { FiatToFiatPseudoOracleFeed } from "../typechain/FiatToFiatPseudoOracleFeed";
 import { IOracleBasedCryptoFiatFeed } from "../typechain/IOracleBasedCryptoFiatFeed";
