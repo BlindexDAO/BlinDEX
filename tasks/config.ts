f2dea6d8bad1fd6fb3c4d494d28b66c67cbd8209
Andrzej Gis <andrzej.gis@widelab.co>
2021-12-28 22:33:56 +0100

    lago-192 removed pools whitelist from tasks

--- tasks/config.ts
+++ tasks/config.ts
@@ -6,7 +6,6 @@ import {
   getUniswapFactory,
   getUniswapRouter,
   getVesting,
-  getWbtc,
   getWeth,
   getBDStableWethPool,
   getBDStableWbtcPool,
@@ -22,6 +21,7 @@ import type { SignerWithAddress } from "hardhat-deploy-ethers/dist/src/signers";
 import { ContractsNames as PriceFeedContractNames } from "../deploy/7_deploy_price_feeds";
 import { cleanStringify } from "../utils/StringHelpers";
 import type { BDStable } from "../typechain/BDStable";
+import { getPools } from "../utils/UniswapPoolsHelpers";
 
 export function load() {
   task("show:be-config").setAction(async (args, hre) => {
@@ -31,15 +31,7 @@ export function load() {
       return { pairAddress: reward.stakingTokenAddress.toLowerCase(), stakingRewardAddress: reward.address.toLowerCase() };
     });
 
-    const {
-      pairs,
-      pairOracles,
-      pairSymbols
-    }: {
-      pairs: { address: string; token0: string; token1: string }[];
-      pairOracles: { pairAddress: string; oracleAddress: string }[];
-      pairSymbols: string[];
-    } = await getPairsOraclesAndSymbols(hre, deployer);
+    const { pairs, pairOracles, pairSymbols } = await getPairsOraclesAndSymbols(hre, deployer);
 
     const swapPairs = pairs.map((pair) => {
       return {
@@ -112,115 +104,56 @@ export function load() {
   });
 }
 
-async function getPairsOraclesAndSymbols(hre: HardhatRuntimeEnvironment, deployer: SignerWithAddress) {
+async function getPairsOraclesAndSymbols(
+  hre: HardhatRuntimeEnvironment,
+  deployer: SignerWithAddress
+): Promise<{
+  pairs: { address: string; token0: string; token1: string }[];
+  pairOracles: { pairAddress: string; oracleAddress: string }[];
+  pairSymbols: string[];
+}> {
   const factory = await getUniswapFactory(hre);
-  const pairsCount = (await factory.allPairsLength()).toNumber();
-  const whitelist = await getPairsWhitelist(hre);
+  const pools = await getPools(hre);
 
-  const pairsWhitelistPromise = whitelist.map(async (pair) => {
-    return {
-      token0Symbol: pair[0].symbol,
-      token1Symbol: pair[1].symbol,
-      token0Address: pair[0].address.toLowerCase(),
-      token1Address: pair[1].address.toLowerCase()
-    };
-  });
+  const pairInfos: {
+    pair: { address: string; token0: string; token1: string };
+    pairOracle: { pairAddress: string; oracleAddress: string };
+    symbol: string;
+  }[] = [];
+  for (const poolPair of pools) {
+    let pairSymbol: string;
+    let oracleAddress: string;
+    let pairAddress: string;
 
-  const pairsWhitelist = await Promise.all(pairsWhitelistPromise);
-  const pairInfos = await Promise.all(
-    [...Array(pairsCount).keys()].map(async (i) => {
-      const pairAddress = await factory.allPairs(i);
-      const pair = (await hre.ethers.getContractAt("UniswapV2Pair", pairAddress)) as UniswapV2Pair;
-      const token0 = (await pair.token0()).toLowerCase();
-      const token1 = (await pair.token1()).toLowerCase();
-
-      const pairSymbols = pairsWhitelist.find(
-        (pair) => (pair.token0Address == token0 && pair.token1Address == token1) || (pair.token0Address == token1 && pair.token1Address == token0)
-      );
-      if (pairSymbols == null) {
-        throw new Error("Please update pair whitelist in getPairsWhitelist method to continue.");
-      }
-
-      let pairSymbol: string;
-      let oracleAddress: string;
-
-      try {
-        pairSymbol = pairSymbols?.token0Symbol + "_" + pairSymbols?.token1Symbol;
-        oracleAddress = (await hre.ethers.getContract(`UniswapPairOracle_${pairSymbol}`, deployer)).address;
-      } catch (e) {
-        pairSymbol = pairSymbols?.token1Symbol + "_" + pairSymbols?.token0Symbol;
-        oracleAddress = (await hre.ethers.getContract(`UniswapPairOracle_${pairSymbol}`, deployer)).address;
-        const temp = pairSymbols.token0Address;
-        pairSymbols.token0Address = pairSymbols.token1Address;
-        pairSymbols.token1Address = temp;
-      }
-
-      if (hre.network.name.toLowerCase() == "rsk") {
-        pairSymbol = pairSymbol.replace("ETH", "RBTC").replace("WBTC", "ETHs");
-      }
-
-      return {
-        pair: { address: pairAddress, token0: pairSymbols.token0Address, token1: pairSymbols.token1Address },
-        pairOracle: { pairAddress: pairAddress, oracleAddress: oracleAddress },
-        symbol: pairSymbol
-      };
-    })
-  );
-
-  const approvedPairs = pairInfos.filter((pairInfo) => {
-    const sortedPair = [pairInfo.pair.token0, pairInfo.pair.token1].sort();
-    for (const wlPair of whitelist) {
-      const wlPairSorted = [wlPair[0].address.toLowerCase(), wlPair[1].address.toLowerCase()].sort();
-      if (wlPairSorted[0] === sortedPair[0] && wlPairSorted[1] === sortedPair[1]) {
-        return true;
-      }
+    try {
+      pairSymbol = poolPair[0].name + "_" + poolPair[1].name;
+      pairAddress = await factory.getPair(poolPair[0].token.address, poolPair[1].token.address);
+      oracleAddress = (await hre.ethers.getContract(`UniswapPairOracle_${pairSymbol}`, deployer)).address;
+    } catch (e) {
+      const tmp = poolPair[0];
+      poolPair[0] = poolPair[1];
+      poolPair[1] = tmp;
+      pairSymbol = poolPair[0].name + "_" + poolPair[1].name;
+      pairAddress = await factory.getPair(poolPair[0].token.address, poolPair[1].token.address);
+      oracleAddress = (await hre.ethers.getContract(`UniswapPairOracle_${pairSymbol}`, deployer)).address;
     }
-    return false;
-  });
 
-  return {
-    pairs: approvedPairs.map((p) => p.pair),
-    pairOracles: approvedPairs.map((p) => p.pairOracle),
-    pairSymbols: approvedPairs.map((p) => p.symbol)
-  };
-}
+    if (hre.network.name.toLowerCase() == "rsk") {
+      pairSymbol = pairSymbol.replace("ETH", "RBTC").replace("WBTC", "ETHs");
+    }
 
-async function getPairsWhitelist(hre: HardhatRuntimeEnvironment) {
-  const weth = await getWeth(hre);
-  const wbtc = await getWbtc(hre);
-  const stables = await getAllBDStables(hre);
-
-  const bdx = await getBdx(hre);
-
-  const whitelist = [
-    [
-      { symbol: "WETH", address: weth.address },
-      { symbol: "BDX", address: bdx.address }
-    ],
-    [
-      { symbol: "WBTC", address: wbtc.address },
-      { symbol: "BDX", address: bdx.address }
-    ]
-  ];
-
-  for (const stable of stables) {
-    const symbol = await stable.symbol();
-
-    whitelist.push([
-      { symbol: "WETH", address: weth.address },
-      { symbol, address: stable.address }
-    ]);
-    whitelist.push([
-      { symbol: "WBTC", address: wbtc.address },
-      { symbol, address: stable.address }
-    ]);
-    whitelist.push([
-      { symbol: "BDX", address: bdx.address },
-      { symbol, address: stable.address }
-    ]);
+    pairInfos.push({
+      pair: { address: pairAddress, token0: poolPair[0].token.address, token1: poolPair[1].token.address },
+      pairOracle: { pairAddress: pairAddress, oracleAddress: oracleAddress },
+      symbol: pairSymbol
+    });
   }
 
-  return whitelist;
+  return {
+    pairs: pairInfos.map((p) => p.pair),
+    pairOracles: pairInfos.map((p) => p.pairOracle),
+    pairSymbols: pairInfos.map((p) => p.symbol)
+  };
 }
 
 async function getStakingsConfig(hre: HardhatRuntimeEnvironment, allowedSwapPairs: { address: string }[]) {
@@ -259,8 +192,6 @@ async function getSwapsConfig(hre: HardhatRuntimeEnvironment) {
   const factory = await getUniswapFactory(hre);
   const pairsCount = (await factory.allPairsLength()).toNumber();
 
-  const whitelist = await getPairsWhitelist(hre);
-
   const pairs = await Promise.all(
     [...Array(pairsCount).keys()].map(async (i) => {
       const pairAddress = await factory.allPairs(i);
@@ -275,18 +206,7 @@ async function getSwapsConfig(hre: HardhatRuntimeEnvironment) {
     })
   );
 
-  const approvedPairs = pairs.filter((pair) => {
-    const sortedPair = [pair.token0.toLowerCase(), pair.token1.toLowerCase()].sort();
-    for (const wlPair of whitelist) {
-      const wlPairSorted = [wlPair[0].address.toLowerCase(), wlPair[1].address.toLowerCase()].sort();
-      if (wlPairSorted[0] === sortedPair[0] && wlPairSorted[1] === sortedPair[1]) {
-        return true;
-      }
-    }
-    return false;
-  });
-
-  return approvedPairs;
+  return pairs;
 }
 
 async function getStablesConfig(hre: HardhatRuntimeEnvironment) {
