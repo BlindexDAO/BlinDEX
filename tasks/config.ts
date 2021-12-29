d546c6d3f7f85781ac3b9d61887bf1e0ea885eee
Andrzej Gis <andrzej.gis@widelab.co>
2021-12-29 01:05:45 +0100

    lago-192 added tokens symbols to config

--- tasks/config.ts
+++ tasks/config.ts
@@ -13,6 +13,7 @@ import {
   getAllBDStables
 } from "../utils/DeployedContractsHelpers";
 import type { UniswapV2Pair } from "../typechain/UniswapV2Pair";
+import type { ERC20 } from "../typechain/ERC20";
 import { d12_ToNumber } from "../utils/NumbersHelpers";
 import type { BdStablePool } from "../typechain/BdStablePool";
 import type { StakingRewards } from "../typechain/StakingRewards";
@@ -26,24 +27,42 @@ import { getPools } from "../utils/UniswapPoolsHelpers";
 export function load() {
   task("show:be-config").setAction(async (args, hre) => {
     const deployer = await getDeployer(hre);
-    const swaps = await getSwapsConfig(hre);
-    const stakingRewards = (await getStakingsConfig(hre, swaps)).map((reward) => {
-      return { pairAddress: reward.stakingTokenAddress.toLowerCase(), stakingRewardAddress: reward.address.toLowerCase() };
+    const stakingRewards = (await getStakingsConfig(hre)).map((reward) => {
+      return {
+        pairAddress: reward.stakingTokenAddress.toLowerCase(),
+        stakingRewardAddress: reward.address.toLowerCase(),
+        token0Address: reward.token0Address,
+        token1Address: reward.token1Address,
+        token0Symbol: reward.token0Symbol,
+        token1Symbol: reward.token1Symbol
+      };
     });
 
-    const { pairs, pairOracles, pairSymbols } = await getPairsOraclesAndSymbols(hre, deployer);
+    const pairOracles = await getPairsOraclesAndSymbols(hre, deployer);
 
-    const swapPairs = pairs.map((pair) => {
+    const swapPairs = pairOracles.map((pairOracle) => {
       return {
-        pairAddress: pair.address.toLowerCase(),
-        token0Address: pair.token0.toLowerCase(),
-        token1Address: pair.token1.toLowerCase()
+        pairAddress: pairOracle.pair.address.toLowerCase(),
+        token0Address: pairOracle.pair.token0.toLowerCase(),
+        token1Address: pairOracle.pair.token1.toLowerCase(),
+        token0Symbol: pairOracle.pair.token0Symbol,
+        token1Symbol: pairOracle.pair.token1Symbol
       };
     });
 
     const mappedPairOracles = pairOracles.map((pairOracle) => {
-      return { pairAddress: pairOracle.pairAddress.toLowerCase(), oracleAddress: pairOracle.oracleAddress.toLowerCase() };
+      return {
+        pairAddress: pairOracle.pair.address.toLowerCase(),
+        oracleAddress: pairOracle.pairOracle.oracleAddress.toLowerCase(),
+        token0Address: pairOracle.pair.token0,
+        token1Address: pairOracle.pair.token1,
+        token0Symbol: pairOracle.pair.token0Symbol,
+        token1Symbol: pairOracle.pair.token1Symbol
+      };
     });
+
+    const pairSymbols = pairOracles.map((po) => po.symbol);
+
     const networkName = hre.network.name.toUpperCase();
 
     const blockchainConfig = {
@@ -83,7 +102,7 @@ export function load() {
 
     const stables = await getStablesConfig(hre);
     const swaps = await getSwapsConfig(hre);
-    const stakings = await getStakingsConfig(hre, swaps);
+    const stakings = await getStakingsConfig(hre);
 
     const blockchainConfig = {
       STABLES: stables,
@@ -104,19 +123,12 @@ export function load() {
   });
 }
 
-async function getPairsOraclesAndSymbols(
-  hre: HardhatRuntimeEnvironment,
-  deployer: SignerWithAddress
-): Promise<{
-  pairs: { address: string; token0: string; token1: string }[];
-  pairOracles: { pairAddress: string; oracleAddress: string }[];
-  pairSymbols: string[];
-}> {
+async function getPairsOraclesAndSymbols(hre: HardhatRuntimeEnvironment, deployer: SignerWithAddress) {
   const factory = await getUniswapFactory(hre);
   const pools = await getPools(hre);
 
   const pairInfos: {
-    pair: { address: string; token0: string; token1: string };
+    pair: { address: string; token0: string; token1: string; token0Symbol: string; token1Symbol: string };
     pairOracle: { pairAddress: string; oracleAddress: string };
     symbol: string;
   }[] = [];
@@ -143,20 +155,22 @@ async function getPairsOraclesAndSymbols(
     }
 
     pairInfos.push({
-      pair: { address: pairAddress, token0: poolPair[0].token.address, token1: poolPair[1].token.address },
+      pair: {
+        address: pairAddress,
+        token0: poolPair[0].token.address,
+        token1: poolPair[1].token.address,
+        token0Symbol: poolPair[0].name,
+        token1Symbol: poolPair[1].name
+      },
       pairOracle: { pairAddress: pairAddress, oracleAddress: oracleAddress },
       symbol: pairSymbol
     });
   }
 
-  return {
-    pairs: pairInfos.map((p) => p.pair),
-    pairOracles: pairInfos.map((p) => p.pairOracle),
-    pairSymbols: pairInfos.map((p) => p.symbol)
-  };
+  return pairInfos;
 }
 
-async function getStakingsConfig(hre: HardhatRuntimeEnvironment, allowedSwapPairs: { address: string }[]) {
+async function getStakingsConfig(hre: HardhatRuntimeEnvironment) {
   const stakingRewardsDistribution = await getStakingRewardsDistribution(hre);
 
   const stakingRewardsAddresses = [];
@@ -174,18 +188,25 @@ async function getStakingsConfig(hre: HardhatRuntimeEnvironment, allowedSwapPair
       const stakingRewards = (await hre.ethers.getContractAt("StakingRewards", address)) as StakingRewards;
       const stakingTokenAddress = await stakingRewards.stakingToken();
 
+      const lpAddress = await stakingRewards.stakingToken();
+      const lp = (await hre.ethers.getContractAt("UniswapV2Pair", lpAddress)) as UniswapV2Pair;
+      const token0Address = await lp.token0();
+      const token1Address = await lp.token1();
+      const token0 = (await hre.ethers.getContractAt("ERC20", token0Address)) as ERC20;
+      const token1 = (await hre.ethers.getContractAt("ERC20", token1Address)) as ERC20;
+
       return {
         address: address,
-        stakingTokenAddress: stakingTokenAddress
+        stakingTokenAddress: stakingTokenAddress,
+        token0Address: token0.address,
+        token1Address: token1.address,
+        token0Symbol: await token0.symbol(),
+        token1Symbol: await token1.symbol()
       };
     })
   );
 
-  const approvedStakings = stakings.filter((s) =>
-    allowedSwapPairs.some((allowedSwap) => s.stakingTokenAddress.toLowerCase() === allowedSwap.address.toLowerCase())
-  );
-
-  return approvedStakings;
+  return stakings;
 }
 
 async function getSwapsConfig(hre: HardhatRuntimeEnvironment) {
