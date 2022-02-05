ce79b2bd2a36b5d31df3e175db533b84fdc68a43
Tom Keidar <tom.keidar@portis.io>
2022-02-05 23:52:09 +0200

    create tasks to easily lock CR at specific value and toggle pause/resume CR calculation

--- test/helpers/bdStable.ts
+++ test/helpers/bdStable.ts
@@ -3,17 +3,21 @@ import { to_d12 } from "../../utils/NumbersHelpers";
 import { getBdEu, getBdUs } from "../../utils/DeployedContractsHelpers";
 import type { BDStable } from "../../typechain/BDStable";
 
-export async function lockBdEuCrAt(hre: HardhatRuntimeEnvironment, targetCR: number) {
+export async function lockBdeuCrAt(hre: HardhatRuntimeEnvironment, targetCR: number) {
+  console.log("Lock BDEU CR at", targetCR);
   const bdEu = await getBdEu(hre);
-  await lockBdStableCR(targetCR, bdEu);
+  await lockBdStableCRAt(targetCR, bdEu);
+  console.log("Lock BDEU completed!");
 }
 
-export async function lockBdUsCrAt(hre: HardhatRuntimeEnvironment, targetCR: number) {
+export async function lockBdusCrAt(hre: HardhatRuntimeEnvironment, targetCR: number) {
+  console.log("Lock BDUS CR at", targetCR);
   const bdUs = await getBdUs(hre);
-  await lockBdStableCR(targetCR, bdUs);
+  await lockBdStableCRAt(targetCR, bdUs);
+  console.log("Lock BDUS completed!");
 }
 
-async function lockBdStableCR(targetCR: number, bdStable: BDStable) {
+async function lockBdStableCRAt(targetCR: number, bdStable: BDStable) {
   if (targetCR < 0) {
     throw new Error("targetCR must >= 0");
   }
@@ -22,5 +26,23 @@ async function lockBdStableCR(targetCR: number, bdStable: BDStable) {
     throw new Error("targetCR must <= 1");
   }
 
-  await bdStable.lockCollateralRatioAt(to_d12(targetCR));
+  await (await bdStable.lockCollateralRatioAt(to_d12(targetCR))).wait();
+}
+
+export async function toggleBdeuCrPaused(hre: HardhatRuntimeEnvironment) {
+  console.log("Toggle BDEU CR Pasued");
+  const bdEu = await getBdEu(hre);
+  await toggleBdStableCrPaused(bdEu);
+  console.log("Toggle BDEU paused completed!");
+}
+
+export async function toggleBdusCrPaused(hre: HardhatRuntimeEnvironment) {
+  console.log("Toggle BDUS CR Pasued");
+  const bdUs = await getBdUs(hre);
+  await toggleBdStableCrPaused(bdUs);
+  console.log("Toggle BDUS paused completed!");
+}
+
+async function toggleBdStableCrPaused(bdStable: BDStable) {
+  await (await bdStable.toggleCollateralRatioPaused()).wait();
 }
