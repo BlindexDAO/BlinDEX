bbf3053acb13680c3339e8e58e53d19a6f4624e8
Or Kazaz <or.kazaz89@gmail.com>
2022-02-06 12:11:31 +0200

    Update tasks/bd-stable.ts

--- tasks/bd-stable.ts
+++ tasks/bd-stable.ts
@@ -2,7 +2,7 @@ import { task } from "hardhat/config";
 import { lockBdeuCrAt, lockBdusCrAt, toggleBdeuCrPaused, toggleBdusCrPaused } from "../test/helpers/bdStable";
 
 export function load() {
-  task("bds:lockBdEuCollateralRatio")
+  task("bds:bdeu:lockCollateralRatio")
     .addPositionalParam("crRatio", "The desired collateral ratio")
     .setAction(async ({ crRatio }, hre) => {
       await lockBdeuCrAt(hre, crRatio);
