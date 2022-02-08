de85d52ffb05f464b723da0ced89148d02ccdaf3
Or Kazaz <or.kazaz89@gmail.com>
2022-02-08 20:50:56 +0200

    Fix PR comments

--- tasks/bdx.ts
+++ tasks/bdx.ts
@@ -10,22 +10,22 @@ import {
   getStakingRewardsDistribution,
   getTreasury
 } from "../utils/DeployedContractsHelpers";
-import { bigNumberToDecimal } from "../utils/NumbersHelpers";
+import { d12_ToNumber, d18_ToNumber } from "../utils/NumbersHelpers";
 import { getAllUniswapPairsData } from "./liquidity-pools";
 
 export function load() {
   task("bdx:show:diagnostics").setAction(async (args, hre) => {
     const bdx = await getBdx(hre);
-    let bdxTotalSupply = await bdx.totalSupply();
-    console.log("BDX Total Supply:", bigNumberToDecimal(bdxTotalSupply, 18).toLocaleString());
+    let bdxTotalSupply_d18 = await bdx.totalSupply();
+    console.log("BDX Total Supply:", d18_ToNumber(bdxTotalSupply_d18).toLocaleString());
 
     const [srd, stables] = await Promise.all([getStakingRewardsDistribution(hre), getAllBDStables(hre)]);
     const operationalTreasuryAddress = hre.network.name === "rsk" ? rskOperationalTreasuryAddress : (await getOperationalTreasury(hre)).address;
     const treasuryAddress = hre.network.name === "rsk" ? rskTreasuryAddress : (await getTreasury(hre)).address;
 
     for (const stable of stables) {
-      console.log(`${await stable.symbol()}: -${bigNumberToDecimal(await bdx.balanceOf(stable.address), 18).toLocaleString()}`);
-      bdxTotalSupply = bdxTotalSupply.sub(await bdx.balanceOf(stable.address));
+      console.log(`${await stable.symbol()}: -${d18_ToNumber(await bdx.balanceOf(stable.address)).toLocaleString()}`);
+      bdxTotalSupply_d18 = bdxTotalSupply_d18.sub(await bdx.balanceOf(stable.address));
     }
 
     const [treasuryBalance, srdBalance, operationalTreasuryBalance] = await Promise.all([
@@ -34,16 +34,16 @@ export function load() {
       bdx.balanceOf(operationalTreasuryAddress)
     ]);
 
-    console.log(`Treasury: -${bigNumberToDecimal(treasuryBalance, 18).toLocaleString()}`);
-    console.log(`Operational Treasury: -${bigNumberToDecimal(operationalTreasuryBalance, 18).toLocaleString()}`);
-    console.log(`Staking Rewards Distribution (anything in staking/vesting): -${bigNumberToDecimal(srdBalance, 18).toLocaleString()}`);
+    console.log(`Treasury: -${d18_ToNumber(treasuryBalance).toLocaleString()}`);
+    console.log(`Operational Treasury: -${d18_ToNumber(operationalTreasuryBalance).toLocaleString()}`);
+    console.log(`Staking Rewards Distribution (anything in staking/vesting): -${d18_ToNumber(srdBalance).toLocaleString()}`);
 
-    bdxTotalSupply = bdxTotalSupply.sub(treasuryBalance).sub(srdBalance).sub(operationalTreasuryBalance);
+    bdxTotalSupply_d18 = bdxTotalSupply_d18.sub(treasuryBalance).sub(srdBalance).sub(operationalTreasuryBalance);
 
     if (hre.network.name === "rsk") {
       const lockingContractBalance = await bdx.balanceOf(bdxLockingContractAddressRSK);
-      bdxTotalSupply = bdxTotalSupply.sub(lockingContractBalance);
-      console.log(`Locking contract: -${bigNumberToDecimal(lockingContractBalance, 18).toLocaleString()}`);
+      bdxTotalSupply_d18 = bdxTotalSupply_d18.sub(lockingContractBalance);
+      console.log(`Locking contract: -${d18_ToNumber(lockingContractBalance).toLocaleString()}`);
     }
 
     async function getBdxInUniswapPools(hre: HardhatRuntimeEnvironment, bdx: BDXShares) {
@@ -68,15 +68,15 @@ export function load() {
       return bdxInPools;
     }
 
-    const bdxTotalSupply_d18 = bigNumberToDecimal(bdxTotalSupply, 18);
+    const bdxTotalSupply = d18_ToNumber(bdxTotalSupply_d18);
     console.log("\n=======================================================================");
-    const bdxPriceUSD = bigNumberToDecimal(await (await getBdUs(hre)).BDX_price_d12(), 12);
+    const bdxPriceUSD = d12_ToNumber(await (await getBdUs(hre)).BDX_price_d12());
     console.log(`BDX Price $${bdxPriceUSD.toLocaleString()}`);
-    console.log(`BDX Marketcap $${(bdxPriceUSD * bdxTotalSupply_d18).toLocaleString()}`);
-    console.log(`BDX Circulating Supply: ${bdxTotalSupply_d18.toLocaleString()}`);
+    console.log(`BDX Marketcap $${(bdxPriceUSD * bdxTotalSupply).toLocaleString()}`);
+    console.log(`BDX Circulating Supply: ${bdxTotalSupply.toLocaleString()}`);
     console.log("\nOut of that amount, we have this amount of BDX in the uniswap pools:");
     const bdxInPools = await getBdxInUniswapPools(hre, bdx);
-    console.log(`\n- And outside of the pools: ${(bdxTotalSupply_d18 - bdxInPools).toLocaleString()}`);
+    console.log(`\n- And outside of the pools: ${(bdxTotalSupply - bdxInPools).toLocaleString()}`);
     console.log("=========================================================================");
   });
 }
