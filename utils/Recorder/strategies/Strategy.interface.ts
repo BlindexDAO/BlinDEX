41362d8b8248859589b5fda8ab7dd46ec752b7a2
Or Kazaz <or.kazaz89@gmail.com>
2022-05-08 17:23:14 +0300

    Update utils/Recorder/strategies/Strategy.interface.ts

--- utils/Recorder/strategies/Strategy.interface.ts
+++ utils/Recorder/strategies/Strategy.interface.ts
@@ -1,7 +1,7 @@
 import { UnsignedTransaction } from "ethers";
 import { ContractReceipt } from "ethers";
 
-// interface for strategy function that somehow executes list of unsigned transaction
+// interface for strategy function that somehow executes a list of unsigned transactions
 // examples: execute one By one, batch all and send as one, send to gnosis safe etc.
 // @txsToExecute: list of UnsignedTransaction to be executed
 // @params?: additional object containing strategy specific parameters
