7d3d366ed2edfd76c69570ef5d944077940fdc88
Or Kazaz <or.kazaz89@gmail.com>
2022-05-10 11:15:01 +0300

    Added comment

--- deploy-scripts/deploy-scripts-constants.ts
+++ deploy-scripts/deploy-scripts-constants.ts
@@ -5,6 +5,8 @@ const deploymentScriptsFolder = "deploy-scripts";
 
 export const chainsDeployScriptsFolders = {
   [chainIds.rsk]: [`${deploymentScriptsFolder}/rsk`],
+  // Our local development is based ont he deployment scripts of RSK, being the native chain of Blindex.
+  // The deployment files there have different conditions basked on the chain
   [chainIds.mainnetFork]: [`${deploymentScriptsFolder}/rsk`],
   [chainIds.arbitrumTestnet]: [`${deploymentScriptsFolder}/arbitrum-testnet`]
 };
