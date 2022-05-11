d64135eac04fb1b80f720ffe3ba87a0e166aa231
Or Kazaz <or.kazaz89@gmail.com>
2022-05-11 13:05:51 +0300

    Add mint bdx script

--- deploy-scripts/arbitrum-test/2-mint-bdx.ts
+++ deploy-scripts/arbitrum-test/2-mint-bdx.ts
@@ -2,17 +2,19 @@ import type { HardhatRuntimeEnvironment } from "hardhat/types";
 import type { DeployFunction } from "hardhat-deploy/types";
 import { to_d18 } from "../../utils/NumbersHelpers";
 import { ethers } from "hardhat";
-import { getBdx, getDeployer, getTreasurySigner } from "../../utils/DeployedContractsHelpers";
-import { deployContract } from "../../utils/DeploymentHelpers";
+import { getBdx, getDeployer, getTreasuryAddress } from "../../utils/DeployedContractsHelpers";
+import { deployContract, printAndWaitOnTransaction } from "../../utils/DeploymentHelpers";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   return await deployContract("Mint BDX", async () => {
     const deployer = await getDeployer(hre);
-    const treasury = await getTreasurySigner(hre);
+    const treasuryAddress = await getTreasuryAddress(hre);
     const bdx = await getBdx(hre);
 
+    const bdx21M = to_d18(21).mul(1e6);
+
     // mint all of the BDX up front to the treasury
-    await (await bdx.connect(deployer).mint(ethers.constants.AddressZero, treasury.address, to_d18(21).mul(1e6))).wait();
+    await printAndWaitOnTransaction(await bdx.connect(deployer).mint(ethers.constants.AddressZero, treasuryAddress, bdx21M));
   });
 };
 
