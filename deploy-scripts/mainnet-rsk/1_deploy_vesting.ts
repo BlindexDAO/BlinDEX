223203d5144b7fda9bbf804d9bfae2667e67a5e4
Andrzej Gis <andrzej.gis@widelab.co>
2022-07-04 18:39:35 +0200

    replaced deployments

---
+++ deploy-scripts/mainnet-rsk/1_deploy_vesting.ts
@@ -0,0 +1,37 @@
+import type { DeployFunction } from "hardhat-deploy/types";
+import type { HardhatRuntimeEnvironment } from "hardhat/types";
+import { getDeployer } from "../../utils/DeployedContractsHelpers";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  console.log("starting deployment: vesting");
+
+  const deployer = await getDeployer(hre);
+  const bdxAddress = "0x6542a10E68cEAc1Fa0641ec0D799a7492795AAC1";
+  const vestingTimeInSeconds = 60 * 60 * 24 * 30 * 9; //9 months
+
+  const vesting_ProxyDeployment = await hre.deployments.deploy("Vesting", {
+    from: deployer.address,
+    proxy: {
+      proxyContract: "OptimizedTransparentProxy",
+      execute: {
+        init: {
+          methodName: "initialize",
+          args: [bdxAddress, deployer.address, deployer.address, vestingTimeInSeconds]
+        }
+      }
+    },
+    contract: "Vesting",
+    args: []
+  });
+
+  console.log("Vesting deployed to: " + vesting_ProxyDeployment.address);
+
+  console.log("finished deployment: vesting");
+
+  return true;
+};
+
+func.id = __filename;
+func.tags = ["Vesting"];
+func.dependencies = [];
+export default func;
---
+++ deploy-scripts/mainnet-rsk/2_staking_rewards_distribution.ts
@@ -0,0 +1,46 @@
+import type { HardhatRuntimeEnvironment } from "hardhat/types";
+import type { DeployFunction } from "hardhat-deploy/types";
+import * as constants from "../../utils/Constants";
+import { getTreasurySigner, getVesting } from "../../utils/DeployedContractsHelpers";
+
+const bdxAddress = "0x6542a10E68cEAc1Fa0641ec0D799a7492795AAC1";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const vesting = await getVesting(hre);
+  const devTreasuryAddress = hre.network.name === "rsk" ? constants.rskDevTreasuryAddress : (await getTreasurySigner(hre)).address;
+
+  const stakingRewardsDistribution_ProxyDeployment = await hre.deployments.deploy("StakingRewardsDistribution", {
+    from: (await hre.getNamedAccounts()).DEPLOYER,
+    proxy: {
+      proxyContract: "OptimizedTransparentProxy",
+      execute: {
+        init: {
+          methodName: "initialize",
+          args: [bdxAddress, vesting.address, devTreasuryAddress, 90]
+        }
+      }
+    },
+    contract: "StakingRewardsDistribution",
+    args: []
+  });
+
+  console.log("Deployed StakingRewardsDistribution: " + stakingRewardsDistribution_ProxyDeployment.address);
+
+  // there is cycle dependency between staking rewards distribution and vesting
+  // once staking rewards distribution is available we override vesting params
+
+  await (await vesting.setFundsProvider(stakingRewardsDistribution_ProxyDeployment.address)).wait();
+  console.log("set funds provider");
+
+  await (await vesting.setVestingScheduler(stakingRewardsDistribution_ProxyDeployment.address)).wait();
+  console.log("set set vesting scheduler");
+
+  console.log("finished deployment: staking rewards distribution");
+
+  // One time migration
+  return true;
+};
+func.id = __filename;
+func.tags = ["StakingRewardsDistribution"];
+func.dependencies = ["Vesting"];
+export default func;
