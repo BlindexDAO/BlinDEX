033a204868aea5ea68836a3ac499a1ddb879d07b
Tom Keidar <hi@tomkeidar.com>
2022-07-13 21:06:16 +0300

    styling improvements

--- tasks/disperse.ts
+++ tasks/disperse.ts
@@ -1,18 +1,18 @@
 import { task } from "hardhat/config";
 import { Disperse } from "../typechain";
-import { getBdx, getUser1 } from "../utils/DeployedContractsHelpers";
+import { getBdx, getDeployer } from "../utils/DeployedContractsHelpers";
 import { printAndWaitOnTransaction } from "../utils/DeploymentHelpers";
 import { to_d18 } from "../utils/NumbersHelpers";
 
 export function load() {
   task("disperse:ether").setAction(async (args, hre) => {
-    // Enter wallet addresses and amounts
+    // Enter wallet addresses and amounts to disperse
     const wallets = [] as string[];
     const amounts = [] as number[];
 
     console.log("Start: disperse ether");
     const amounts_d18 = amounts.map(amount => to_d18(+amount));
-    const user = await getUser1(hre);
+    const user = await getDeployer(hre);
     const disperse = (await hre.ethers.getContract("Disperse", user)) as Disperse;
 
     console.log(`User ether balance is: ${(await user.getBalance()).toString()}`);
@@ -23,7 +23,7 @@ export function load() {
 
     console.log("disperseEther");
     const value = amounts_d18.reduce((totalValue, amount) => totalValue.add(amount), hre.ethers.constants.Zero);
-    await printAndWaitOnTransaction(await disperse.disperseEther(wallets, amounts_d18, { value }));
+    await printAndWaitOnTransaction(await disperse.disperseEther(wallets, amounts, { value }));
 
     console.log(`User ether balance is: ${(await user.getBalance()).toString()}`);
     for (let index = 0; index < wallets.length; index++) {
@@ -33,20 +33,20 @@ export function load() {
   });
 
   task("disperse:bdx").setAction(async (args, hre) => {
-    // Enter wallet addresses and amounts
+    // Enter wallet addresses and amounts to disperse
     const wallets = [] as string[];
     const amounts = [] as number[];
 
     console.log("Start: disperse erc20");
     const amounts_d18 = amounts.map(amount => to_d18(+amount));
-    const user = await getUser1(hre);
+    const user = await getDeployer(hre);
     const disperse = (await hre.ethers.getContract("Disperse", user)) as Disperse;
     const bdx = await getBdx(hre);
 
     console.log(`User BDX balance is: ${(await bdx.balanceOf(user.address)).toString()}`);
     for (let index = 0; index < wallets.length; index++) {
       const walletAddress = wallets[index];
-      console.log(`Wallet ${walletAddress} balance is: ${(await bdx.balanceOf(walletAddress)).toString()}`);
+      console.log(`Wallet ${walletAddress} BDX balance is: ${(await bdx.balanceOf(walletAddress)).toString()}`);
     }
 
     console.log("Allowance");
@@ -59,7 +59,7 @@ export function load() {
     console.log(`User BDX balance is: ${(await bdx.balanceOf(user.address)).toString()}`);
     for (let index = 0; index < wallets.length; index++) {
       const walletAddress = wallets[index];
-      console.log(`Wallet ${walletAddress} balance is: ${(await bdx.balanceOf(walletAddress)).toString()}`);
+      console.log(`Wallet ${walletAddress} BDX balance is: ${(await bdx.balanceOf(walletAddress)).toString()}`);
     }
   });
 }
